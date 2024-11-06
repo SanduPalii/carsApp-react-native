7fd247775bc91231593a5fae92ea6aa4618da876
Sandu Palii <palii.sandu@gmail.com>
2024-11-06 09:18:15 +0200

    Initial commit after reinitialization

---
+++ components/InputField.tsx
@@ -0,0 +1,65 @@
+import {
+  View,
+  Text,
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+  Image,
+  TextInput,
+  Platform,
+  Keyboard,
+} from 'react-native'
+import React from 'react'
+import { twMerge } from 'tailwind-merge'
+import { InputFieldProps } from '@/types/type'
+
+const ImputField = ({
+  label,
+  labelStyle,
+  icon,
+  secureTextEntry = false,
+  containerStyle,
+  inputStyle,
+  iconStyle,
+  className,
+  ...props
+}: InputFieldProps) => {
+  return (
+    <KeyboardAvoidingView
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+    >
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <View className="my-2 w-full">
+          <Text
+            className={twMerge('text-lg font-JakartaSemiBold mb-3', labelStyle)}
+          >
+            {label}
+          </Text>
+          <View
+            className={twMerge(
+              'flex flex-row justify-start items-center relative bg-neutral-100 rounded-full border border-neutral-100 focus:border-primary-500',
+              containerStyle
+            )}
+          >
+            {icon ? (
+              <Image
+                source={icon}
+                className={twMerge('w-6 h-6 ml-4', iconStyle)}
+              />
+            ) : null}
+            <TextInput
+              className={twMerge(
+                'rounded-full p-4 font-JakartaSemiBold text-[15px] flex-1 text-left ',
+                inputStyle,
+                className
+              )}
+              secureTextEntry={secureTextEntry}
+              {...props}
+            />
+          </View>
+        </View>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
+  )
+}
+
+export default ImputField
