4fe9aa0889d4f1298a6315c0302f1cff5eb8ddf0
Sandu Palii <palii.sandu@gmail.com>
2024-11-06 12:35:54 +0200

    neon database connected

--- app/(api)/user+api.ts
+++ app/(api)/user+api.ts
@@ -1,7 +1,27 @@
 import { neon } from '@neondatabase/serverless'
 
-const sql = neon(
-  'postgresql://jsm_cars_owner:8O9YbtkCFGwR@ep-black-dream-a25r8jyv.eu-central-1.aws.neon.tech/jsm_cars?sslmode=require'
-)
-
-const posts = await sql('SELECT * FROM posts')
+export async function POST(request: Request) {
+  try {
+    const sql = neon(`${process.env.DATABASE_URL}`)
+    const { name, email, clerkId } = await request.json()
+    if (!name || !email || !clerkId) {
+      return Response.json({ error: 'Missing required field' }, { status: 400 })
+    }
+    const response = await sql`
+  INSERT INTO users(
+    name, 
+    email,
+    clerk_id
+  )
+  VALUES(
+    ${name},
+    ${email},
+    ${clerkId}
+  )
+  `
+    return new Response(JSON.stringify({ data: response }), { status: 201 })
+  } catch (error) {
+    console.log(error)
+    return Response.json({ error: error }, { status: 500 })
+  }
+}
